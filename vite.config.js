f87674ed1d62c9b3c40e6c41e03153c0d0051c50
GonzaloAlvaroS <gonzalo02suarez@gmail.com>
2024-10-09 22:17:19 -0500

    alo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/proyecto4/',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',      
+    port: process.env.PORT || 3000, 
+  }
 })
